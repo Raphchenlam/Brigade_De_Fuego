f7e525e1a89b28f37e0a5bdefd18a04cc0f652d9
Marchand007 <m.marchand22@hotmail.com>
2023-08-24 16:14:41 -0400

    Commit inital frontend

---
+++ brigade_frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-app>
+        <EnteteNavigation />
+        <v-main>
+            <router-view></router-view>
+        </v-main>
+    </v-app>
+</template>
+
+<script>
+
+import EnteteNavigation from './components/EnteteNaviguation.vue';
+
+export default {
+    components: {
+        EnteteNavigation
+    }
+}
+
+</script>
