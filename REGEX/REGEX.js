fe2b3cdfe109e7430a9af1256eaeb3ec87f8cb68
Raphchenlam <122387105+Raphchenlam@users.noreply.github.com>
2023-09-14 11:28:33 -0400

    Delete REGEX/REGEX.js

--- REGEX/REGEX.js
+++
@@ -1,8 +0,0 @@
-export const validEmployeeNumber = /^\d{4}$/;
-export const validName = /^([A-Z][a-z]{1,}(?:[- ][A-Z][a-z]{1,})?)$/;
-export const validColorHexcode = /^#([A-F0-9]{6}|[A-F0-9]{3})$/;
-export const validHourlyRate = /^[1-9]\d{1,2}\.\d{2}$/;
-export const validBarcodeNumber = /^\d{16}$/;
-export const validEmail = /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.(com|ca|fr)$/;
-export const validPhoneNumber = /^\d{3}-\d{3}-\d{4}$/;
-export const validSkillPoints = /^(?:[1-9]|10)$/;
