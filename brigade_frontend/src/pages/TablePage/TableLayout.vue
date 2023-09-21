08e219e91be9b5cea8050a5ac92d519e314c2eca
DBeaudry03 <122543753+DBeaudry03@users.noreply.github.com>
2023-09-21 15:45:47 -0400

    Update TableLayout.vue

--- brigade_frontend/src/pages/TablePage/TableLayout.vue
+++ brigade_frontend/src/pages/TablePage/TableLayout.vue
@@ -1,8 +1,9 @@
 <template>
     <v-sheet class="ma-2 w-100">
         <v-card height="75dvh">
-            <tableIcon class="d-inline" v-for="table in tableList" :key="table.number" :tableNumber="table.number" :tableCapacity="table.capacity" :tableIsActive="table.isActive" :isAssign="isAssign"></tableIcon>
-            <!-- <brigade_table4 class="assign"></brigade_table4> -->
+            <tableIcon class="d-inline" v-for="table in tableList" :key="table.number" :tableNumber="table.number"
+                :tableCapacity="table.capacity" :tableIsActive="table.isActive" :isAssign="isAssign">
+            </tableIcon>
 
         </v-card>
     </v-sheet>
@@ -16,19 +17,19 @@ export default {
     components: {
         tableIcon
     },
-    inject:['tableList', 'assignationList'],
+    inject: ['tableList', 'assignationList'],
     data() {
         return {
-            key:null,
+            key: null,
             //hasReservation: false,
-            isAssign:false
+            isAssign: false
         }
     },
-    computed:{
-        isAssign(){
+    computed: {
+        isAssign() {
             if (this.assignationList.find(a => a.tableNumber === this.key)) {
                 console.log("key : " + this.key)
-                return true;
+                return this.isAssign =true;
             }
         }
     }
