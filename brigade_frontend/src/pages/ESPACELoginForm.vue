b5769cd6fa5f675ece95b276643461f3b67622b9
FrancisMaynard <francismaynard99@gmail.com>
2023-09-27 11:37:42 -0400

    petite corrections EspaceLogin

--- brigade_frontend/src/pages/ESPACELoginForm.vue
+++ brigade_frontend/src/pages/ESPACELoginForm.vue
@@ -3,9 +3,9 @@
         <v-sheet class="ma-2" max-width="40rem">
             <v-form @submit.prevent="login" validate-on="submit lazy" ref="loginForm">
                 <v-text-field v-model="employeeNumber" label="Numero d'employé" :counter="4" maxlength="4"
-                    :rules="[rules.required, rules.validPassword]" density="compact" autofocus></v-text-field>
+                    :rules="[rules.required, rules.validatePassword]" density="compact" autofocus></v-text-field>
                 <v-text-field v-model="password" label="Mot de passe" type="password" 
-                :rules="[rules.required, rules.validPassword]" density="compact"></v-text-field>
+                :rules="[rules.required, rules.validatePassword]" density="compact"></v-text-field>
                 <v-btn type="submit" :disabled="!employeeNumber || !password">Se connecter</v-btn>
             </v-form>
             <div class="text-body ma-3">Mot de passe oublié ?&nbsp;
@@ -25,10 +25,10 @@ export default {
         return {
             employeeNumber: '',
             password: '',
-            validPassword: true,
+            validPassword: false,
             rules: {
                 required: value => !!value || "Le champ est requis",
-                validPassword: () => this.passwordValid || "Numéro d'employé ou mot de passe invalide"
+                validatePassword: () => this.validPassword || "Numéro d'employé ou mot de passe invalide"
             }
         };
     },
@@ -36,11 +36,11 @@ export default {
         login()
         {
             userSession.login(this.employeeNumber, this.password).then(() => {
-                this.passwordValid = true;
+                this.validPassword = true;
                 this.$refs.loginForm.validate();
                 this.$router.push('/espace/dashboard');
             }).catch(authError => {
-                this.passwordValid = false;
+                this.validPassword = false;
                 this.$refs.loginForm.validate();
                 alert(authError.message);
             });
