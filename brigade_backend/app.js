fa504b60a210449409b5b925e1858cebcc4570e1
Marchand007 <m.marchand22@hotmail.com>
2023-11-07 11:11:01 -0500

    Update app.js

--- brigade_backend/app.js
+++ brigade_backend/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const HttpError = require('./HTTPEr');
+const HttpError = require('./HTTPError');
 
 const passport = require('passport');
 const BasicStrategy = require('passport-http').BasicStrategy;
