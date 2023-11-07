a2021a713217e5a7fbd354335d38c60f6a555891
Marchand007 <m.marchand22@hotmail.com>
2023-11-07 11:13:16 -0500

    update encore calisse

---
+++ brigade_backend/db_config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+};
+
+/*
+ user: 'backend_user',
+    host: 'localhost',
+    port: '5432',
+    database:'brigade',
+    password:'12345'
+*/
