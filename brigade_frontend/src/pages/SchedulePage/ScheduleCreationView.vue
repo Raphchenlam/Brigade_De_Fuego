9bf1d38aa8afc3bdc418af80d745594283c380b2
Marchand007 <m.marchand22@hotmail.com>
2023-11-07 18:26:09 -0500

    Update ScheduleCreationView.vue

--- brigade_frontend/src/pages/SchedulePage/ScheduleCreationView.vue
+++ brigade_frontend/src/pages/SchedulePage/ScheduleCreationView.vue
@@ -1029,6 +1029,8 @@ export default {
                         const dateKey = element.date;
                         const shiftName = element.shiftName;
                         const mapping = this.datePropertyMapping[dateKey];
+                        console.log("this.datePropertyMapping",this.datePropertyMapping)
+                        console.log("this.datePropertyMapping[dateKey]",this.datePropertyMapping[dateKey])
                         console.log("mapping",mapping)
                         console.log("mapping[shiftName]",mapping[shiftName])
                         if (mapping && mapping[shiftName])
